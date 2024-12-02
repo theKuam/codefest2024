15454f0eb567ceca2b731a9c83bf5b22c936b5ac
Ngo Minh Hai <ngominh.hai96@gmail.com>
2024-12-02 17:11:43 +0700

    optimize performance

--- models/weapon_wind.py
+++ models/weapon_wind.py
@@ -2,6 +2,5 @@ class WeaponWind:
     def __init__(self, response):
         self.player_id = response.get('playerId')
         self.current_position = (response.get('currentRow'), response.get('currentCol'))
-        raw_destination = response.get('destination', {})
-        self.destination = (raw_destination.get('row'), raw_destination.get('col'))
+        self.direction = response.get('direction')
         self.create_at = response.get('createAt')
